3a0b087b777449d6c1f058ec7acf260e255a52b6
Khaerul Umam <khaerul.umam@digitalsekuriti.id>
2023-05-25 21:30:19 +0700

    init init

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from "express";
-import db from "./config/database.js";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import db from './config/database.js';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
-import Users from "./models/userModels.js";
-import Product from "./models/productModel.js";
-import router from "./routes/user.js";
+import Users from './models/userModels.js';
+import Product from './models/productModel.js';
+import router from './routes/user.js';
 
 const app = express();
 const { PORT } = process.env;
@@ -20,13 +20,13 @@ const startServer = async () => {
     await db.authenticate();
     await Users.sync();
     await Product.sync();
-    console.log("Connected to the database");
+    console.log('Connected to the database');
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error('Unable to connect to the database:', error);
     process.exit(1);
   }
 };
